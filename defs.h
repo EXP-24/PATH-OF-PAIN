1e23913bf9ecc74f8a78b8297ff82688b43e2a52
EXP-24 <duccuongntt@gmail.com>
2025-04-22 22:03:47 +0700

    Create defs.h

---
+++ defs.h
@@ -0,0 +1,19 @@
+#ifndef _DEFS__H
+#define _DEFS__H
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 800;
+const char* WINDOW_TITLE = "Path of pain";
+const int PLAYER_HEIGHT = 128;
+const int PLAYER_WIDTH = 128;
+const int GRAVITY = 1;
+const int JUMP_STRENGTH = 15;
+const int PLAYER_SPEED = 7;
+const int MAP_WIDTH = 200;
+const int MAP_HEIGHT = 32;
+const int STARTX = 100;
+const int STARTY = 700;
+const int ENDX = 190*32;
+const int ENDY = 22*32;
+#endif
+
