37074ca091ec871489ba6f399c43c8824afce2d4
EXP-24 <duccuongntt@gmail.com>
2025-04-25 21:56:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,136 @@
-#include<iostream>
+#include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL_ttf.h>
 #include "graphics.h"
 #include "sprites.h"
 #include "defs.h"
 #include "game.h"
 #include "map.h"
+#include "menu.h"
 
 using namespace std;
 int main(int argc, char* args[]) {
 
     Graphics graphics;
     graphics.init();
-    Mix_Music *gMusic = graphics.loadMusic("include/m&s/White Palace.mp3");
-    graphics.play(gMusic);
-    Mix_VolumeMusic(50);
 
-    Map mapGame;
-    mapGame.init(graphics);
+    bool running = true;
+    while(running){
+        mainMenu menu;
+        menu.init(graphics);
 
-    Character player;
-    player.init(graphics);
-
-    int map[MAP_HEIGHT][MAP_WIDTH];
-    mapGame.loadMap("include/map.txt", map);
-    bool quit = false;
-    SDL_Event e;
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            } else{
-                player.handleEvent(graphics, e);
+        Mix_Music *titleSong = graphics.loadMusic("include/m&s/Title Theme.mp3");
+        graphics.play(titleSong);
+        Mix_VolumeMusic(100);
+        SDL_Event e;
+        bool inMenu = true;
+        while (inMenu) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT ){
+                    running = false;
+                    inMenu = false;
+                }
+                else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                    int mouseX, mouseY;
+                    SDL_GetMouseState(&mouseX, &mouseY);
+                    if (menu.playClicked(mouseX, mouseY)){
+                        inMenu = false;
+                    }
+                    if (menu.quitClicked(mouseX, mouseY)){
+                        running = false;
+                        inMenu = false;
+                    }
+                }
             }
+
+            graphics.prepareScene();
+            menu.render(graphics);
+            graphics.presentScene();
+            SDL_Delay(30);
         }
-        player.move(graphics, map);
-        player.update();
-        mapGame.updateCamera(player.x, player.y);
 
-        graphics.prepareScene();
-        mapGame.render(graphics, map);
-        player.render(graphics, mapGame.camera.x, mapGame.camera.y);
+        menu.close();
+        if (titleSong != nullptr) Mix_FreeMusic(titleSong);
 
-        graphics.presentScene();
-        SDL_Delay(30);
+        if (!running) break;
+
+        Map mapGame;
+        mapGame.init(graphics);
+
+        Character player;
+        player.init(graphics);
+
+        PauseMenu pMenu;
+        pMenu.init(graphics);
+
+        //Load map va nhac nen game
+        int map[MAP_HEIGHT][MAP_WIDTH];
+        mapGame.loadMap("include/map.txt", map);
+
+        Mix_Music *gMusic = graphics.loadMusic("include/m&s/White Palace.mp3");
+        graphics.play(gMusic);
+
+        bool pause = false;
+        bool quit = false;
+        while (!quit) {
+            while (SDL_PollEvent(&e) != 0) {
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                    running = false;
+                }
+
+                else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE){
+                    pause = !pause;
+                    if (!pause){
+                        player.velX = 0;
+                        player.velY = 0;
+                    }
+                }
+
+                else if (e.type == SDL_MOUSEBUTTONDOWN){
+                    int mouseX, mouseY;
+                    SDL_GetMouseState(&mouseX, &mouseY);
+                    if (pMenu.resumeClicked(mouseX, mouseY)){
+                        pause = false;
+                        player.velX = 0;
+                        player.velY = 0;
+                    }
+                    else if (pMenu.backClicked(mouseX, mouseY)){
+                        quit = true;
+                        inMenu = true;
+
+                    }
+                }
+
+                if (!pause){
+                    player.handleEvent(graphics, e);
+                }
+            }
+
+            if (!pause){
+                player.move(graphics, map);
+                player.update();
+                mapGame.updateCamera(player.x, player.y);
+            }
+
+            graphics.prepareScene();
+            mapGame.render(graphics, map);
+            player.render(graphics, mapGame.camera.x, mapGame.camera.y);
+            if (pause){
+                pMenu.render(graphics);
+            }
+            graphics.presentScene();
+            SDL_Delay(30);
+        }
+
+        if (gMusic != nullptr) Mix_FreeMusic(gMusic);
+        pMenu.close();
+        player.close();
+        mapGame.close();
     }
-    if (gMusic != nullptr) Mix_FreeMusic(gMusic);
 
-    player.close();
-    mapGame.close();
     graphics.quit();
     return 0;
 }
