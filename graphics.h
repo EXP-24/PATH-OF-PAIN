724aaecafad383e1ce39f54b2ea288c538a26cd8
EXP-24 <duccuongntt@gmail.com>
2025-04-25 21:55:57 +0700

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,10 +1,10 @@
-
 #ifndef _GRAPHICS__H
 #define _GRAPHICS__H
 
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL_ttf.h>
 #include "defs.h"
 #include <vector>
 
@@ -27,14 +27,13 @@ struct Sprite {
             clips.push_back(clip);
         }
     }
-    bool tick() {
+
+    void tick() {
         Uint32 currentTime = SDL_GetTicks();
         if (currentTime - lastFrameTime >= frameDelay) {
             currentFrame = (currentFrame + 1) % clips.size();
             lastFrameTime = currentTime;
-            return currentFrame == 0;
         }
-        return false;
     }
 
     const SDL_Rect* getCurrentClip() const {
@@ -42,11 +41,26 @@ struct Sprite {
     }
 };
 
+struct ScrollingBackground {
+    SDL_Texture* texture;
+    int scrollingOffset = 0;
+    int width, height;
+
+    void setTexture(SDL_Texture* _texture) {
+        texture = _texture;
+        SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+    }
+
+    void scroll(int distance) {
+        scrollingOffset -= distance;
+        if( scrollingOffset < 0 ) { scrollingOffset = width; }
+    }
+};
 
 struct Graphics {
     SDL_Renderer *renderer;
 	SDL_Window *window;
-
+    //Khởi tạo cửa sổ
 	void logErrorAndExit(const char* msg, const char* error)
     {
         SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
@@ -75,26 +89,27 @@ struct Graphics {
         SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
         SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        //Initialize SDL_mixer
-        if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-        {
+        if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 ){
             logErrorAndExit( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
         }
-    }
 
-	void prepareScene()
-    {
+        if (TTF_Init() == -1){
+            logErrorAndExit("SDL_ttf could not initialize! SDL_ttf Error: ",
+            TTF_GetError());
+        }
+    }
+    //Xử lí ảnh
+	void prepareScene() {
         SDL_RenderClear(renderer);
         SDL_RenderCopy( renderer, NULL, NULL, NULL);
     }
 
-    void presentScene()
-    {
+    void presentScene() {
         SDL_RenderPresent(renderer);
     }
 
-    SDL_Texture *loadTexture(const char *filename)
-    {
+    SDL_Texture *loadTexture(const char *filename) {
+
         SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
 
         SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
@@ -104,8 +119,7 @@ struct Graphics {
         return texture;
     }
 
-    void renderTexture(SDL_Texture *texture, int x, int y)
-    {
+    void renderTexture(SDL_Texture *texture, int x, int y) {
         SDL_Rect dest;
 
         dest.x = x;
@@ -115,7 +129,6 @@ struct Graphics {
         SDL_RenderCopy(renderer, texture, NULL, &dest);
     }
 
-
     void render(int x, int y, const Sprite& sprite, bool flip) {
         const SDL_Rect* clip = sprite.getCurrentClip();
         SDL_Rect renderQuad = {x, y, clip->w, clip->h};
@@ -123,16 +136,22 @@ struct Graphics {
         SDL_RenderCopyEx(renderer, sprite.texture, clip, &renderQuad, 0, NULL, flipType);
     }
 
-    void quit()
-    {
+    void renderBackground(const ScrollingBackground& background) {
+        renderTexture(background.texture, background.scrollingOffset, 0);
+        renderTexture(background.texture, background.scrollingOffset - background.width, 0);
+    }
+
+    void quit() {
         IMG_Quit();
         Mix_Quit();
+        TTF_Quit();
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
     }
-    Mix_Music *loadMusic(const char* path)
-    {
+
+    //Xử lí nhạc
+    Mix_Music *loadMusic(const char* path) {
         Mix_Music *gMusic = Mix_LoadMUS(path);
         if (gMusic == nullptr) {
             SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
@@ -140,8 +159,8 @@ struct Graphics {
         }
         return gMusic;
     }
-    void play(Mix_Music *gMusic)
-    {
+
+    void play(Mix_Music *gMusic) {
         if (gMusic == nullptr) return;
 
         if (Mix_PlayingMusic() == 0) {
@@ -160,10 +179,36 @@ struct Graphics {
         }
         return gChunk;
     }
+
     void play(Mix_Chunk* gChunk) {
         if (gChunk != nullptr) {
             Mix_PlayChannel( -1, gChunk, 0 );
         }
+    }
+
+    //Xử lí font
+    TTF_Font* loadFont(const char* path, int size) {
+        TTF_Font* gFont = TTF_OpenFont( path, size );
+        if (gFont == nullptr) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load font %s", TTF_GetError());
+        }
+        return gFont;
+    }
+
+    SDL_Texture* renderText(const char* text, TTF_Font* font, SDL_Color textColor) {
+        SDL_Surface* textSurface = TTF_RenderText_Solid( font, text, textColor );
+        if( textSurface == nullptr ) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Render text surface %s", TTF_GetError());
+            return nullptr;
+        }
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface( renderer, textSurface );
+        if( texture == nullptr ) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Create texture from text %s", SDL_GetError());
+        }
+
+        SDL_FreeSurface( textSurface );
+        return texture;
     }
 };
 
